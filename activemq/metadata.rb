f2a5fd815d9b831d96144061a5db773ee51c0ce9
Chris Fordham <chris@xhost.com.au>
2011-11-16 17:33:21 +1100

    Remove required for attributes.

--- activemq/metadata.rb
+++ activemq/metadata.rb
@@ -18,20 +18,17 @@ recipe "activemq::default", "Installs/configures activemq."
 attribute "activemq/mirror",
   :display_name => "ActiveMQ mirror",
   :description => "download URL up to the activemq/apache-activemq directory.",
-  :required => "required",
   :default => "http://apache.mirrors.tds.net",
   :recipes => [ "activemq::default" ]
 
 attribute "activemq/version",
   :display_name => "ActiveMQ version",
   :description => "ActiveMQ version to install.",
-  :required => "required",
   :default => "5.5.0",
   :recipes => [ "activemq::default" ]
 
 attribute "activemq/home",
   :display_name => "ActiveMQ deploy directory",
   :description => "Directory to deploy to (/opt by default",
-  :required => "required",
   :default => "/opt",
   :recipes => [ "activemq::default" ]
