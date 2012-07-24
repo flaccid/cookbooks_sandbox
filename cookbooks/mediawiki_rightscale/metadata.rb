719e695b0bc31c2e5075d2f39cf3aac7d7c3534d
Chris Fordham <chris.fordham@rightscale.com>
2012-07-24 22:02:24 +1000

    Add trailing period to description.

--- cookbooks/mediawiki_rightscale/metadata.rb
+++ cookbooks/mediawiki_rightscale/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Chris Fordham"
 maintainer_email "chris@xhost.com.au"
 license          "Apache 2.0"
-description      "Installs/Configures mediawiki_application on RightScale"
+description      "Installs/Configures mediawiki_application on RightScale."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
